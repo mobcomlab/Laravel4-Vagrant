f16f28db2819f3882e51951b02278d2171f479f8
Ant <antonyharfield@gmail.com>
2015-07-12 12:48:25 +0700

    Fixed schedule.

--- www/app/Console/Kernel.php
+++ www/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('pull:humidtemp')->everyMinute();
+		$schedule->command('pull:humidtemp')->cron('* * * * *');
 		$schedule->command('pull:power')->everyFiveMinutes();
 	}
 
