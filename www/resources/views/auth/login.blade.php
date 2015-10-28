6140b813e8eec257ec24a13911bd27d30e6d2a33
Tanakorn Amnoeywiboolpol <l3irdWings@gmail.com>
2015-10-28 16:27:34 +0700

    fix bug can't login

--- www/resources/views/auth/login.blade.php
+++ www/resources/views/auth/login.blade.php
@@ -1,6 +1,25 @@
 @extends('app')
 
 @section('content')
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">ET Building: Climate Comfort Monitoring</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="/">SC5-214</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
