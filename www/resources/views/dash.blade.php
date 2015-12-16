ac26d039805db4f202896dc11d0ca79716392631
Tanakorn Amnoeywiboolpol <l3irdWings@gmail.com>
2015-12-16 10:24:57 +0700

    design

--- www/resources/views/dash.blade.php
+++ www/resources/views/dash.blade.php
@@ -11,7 +11,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">ET Building: Climate Comfort Monitoring</a>
+				<a class="navbar-brand" href="/">ET Building: Climate Comfort Monitoring</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
